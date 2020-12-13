500f74007d342a27c4a91b79d392609029a1e77e
Vitalii <vitaliy.rysich@gmail.com>
2020-12-13 12:53:42 +0100

    Day 12 refactoring

--- src/main/java/solutions/day12/navigation/WaypointNavigation.java
+++ src/main/java/solutions/day12/navigation/WaypointNavigation.java
@@ -2,88 +2,37 @@ package solutions.day12.navigation;
 
 public class WaypointNavigation extends Navigation{
 
-    private int waypointEast = 10;
-    private int waypointWest;
-    private int waypointNorth = 1;
-    private int waypointSouth;
+    private int waypointEastWest = 10;
+    private int waypointNorthSouth = 1;
 
 
     @Override
     public void actionN(int val) {
-        if(waypointSouth != 0){
-            if(waypointSouth > val){
-                waypointSouth = waypointSouth - val;
-            }
-            else {
-                waypointNorth += val - waypointSouth;
-                waypointSouth = 0;
-            }
-        }
-        else {
-            waypointNorth += val;
-        }
+        waypointNorthSouth +=val;
     }
 
     @Override
     public void actionS(int val) {
-        if(waypointNorth != 0){
-            if(waypointNorth > val){
-                waypointNorth = waypointNorth - val;
-            }
-            else {
-                waypointSouth += val - waypointNorth;
-                waypointNorth = 0;
-            }
-        }
-        else {
-            waypointSouth += val;
-        }
+        waypointNorthSouth -=val;
     }
 
     @Override
     public void actionE(int val) {
-        if(waypointWest != 0){
-            if(waypointWest > val){
-                waypointWest = waypointWest - val;
-            }
-            else {
-                waypointEast += val - waypointWest;
-                waypointWest = 0;
-            }
-        }
-        else {
-            waypointEast += val;
-        }
+        waypointEastWest += val;
     }
 
     @Override
     public void actionW(int val) {
-        if(waypointEast != 0){
-            if(waypointEast > val){
-                waypointEast = waypointEast - val;
-            }
-            else {
-                waypointWest += val - waypointEast;
-                waypointEast = 0;
-            }
-        }
-        else {
-            waypointWest += val;
-        }
+        waypointEastWest -=val;
     }
 
     @Override
     public void actionL(int val) {
         for (int i=0; i < val/90; i++) {
-            int tempE = waypointEast,
-                    tempW = waypointWest,
-                    tempN = waypointNorth,
-                    tempS = waypointSouth;
+            int tempEW = waypointEastWest;
 
-            waypointEast = tempS;
-            waypointSouth = tempW;
-            waypointWest = tempN;
-            waypointNorth = tempE;
+            waypointEastWest = -1 * waypointNorthSouth;
+            waypointNorthSouth = tempEW;
         }
     }
 
@@ -91,55 +40,32 @@ public class WaypointNavigation extends Navigation{
     public void actionR(int val) {
 
         for (int i=0; i < val/90; i++) {
-            int tempE = waypointEast,
-                    tempW = waypointWest,
-                    tempN = waypointNorth,
-                    tempS = waypointSouth;
+            int tempEW = waypointEastWest;
 
-            waypointEast = tempN;
-            waypointSouth = tempE;
-            waypointWest = tempS;
-            waypointNorth = tempW;
+            waypointEastWest = waypointNorthSouth;
+            waypointNorthSouth = -1 * tempEW;
         }
     }
 
     @Override
     public void actionF(int val) {
-        if(waypointEast != 0) eastWestPos += val * waypointEast;
-        if(waypointWest != 0) eastWestPos -= val * waypointWest;
-        if(waypointNorth != 0) northSouthPos += val * waypointNorth;
-        if(waypointSouth != 0) northSouthPos -= val * waypointSouth;
+        eastWestPos += val * waypointEastWest;
+        northSouthPos += val * waypointNorthSouth;
     }
 
-    public int getWaypointEast() {
-        return waypointEast;
+    public int getWaypointEastWest() {
+        return waypointEastWest;
     }
 
-    public void setWaypointEast(int waypointEast) {
-        this.waypointEast = waypointEast;
+    public void setWaypointEastWest(int waypointEastWest) {
+        this.waypointEastWest = waypointEastWest;
     }
 
-    public int getWaypointWest() {
-        return waypointWest;
+    public int getWaypointNorthSouth() {
+        return waypointNorthSouth;
     }
 
-    public void setWaypointWest(int waypointWest) {
-        this.waypointWest = waypointWest;
-    }
-
-    public int getWaypointNorth() {
-        return waypointNorth;
-    }
-
-    public void setWaypointNorth(int waypointNorth) {
-        this.waypointNorth = waypointNorth;
-    }
-
-    public int getWaypointSouth() {
-        return waypointSouth;
-    }
-
-    public void setWaypointSouth(int waypointSouth) {
-        this.waypointSouth = waypointSouth;
+    public void setWaypointNorthSouth(int waypointNorthSouth) {
+        this.waypointNorthSouth = waypointNorthSouth;
     }
 }
